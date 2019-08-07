5235d57fe1a3d18b8c22425bb06bab5836d89a84
unknown <mariia>
2019-08-07 09:43:43 +0300

    Updates

---
+++ precache-manifest.07824716209a553c37aa2ef4610e9015.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d385df0349e57acad1332c2c1f66e6",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "6ed63f2e4244ae9de03f",
+    "url": "/react_phone-catalog/static/css/main.70e6fef0.chunk.css"
+  },
+  {
+    "revision": "150298c2983476d51719",
+    "url": "/react_phone-catalog/static/js/2.454e4301.chunk.js"
+  },
+  {
+    "revision": "6ed63f2e4244ae9de03f",
+    "url": "/react_phone-catalog/static/js/main.d1d60924.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
